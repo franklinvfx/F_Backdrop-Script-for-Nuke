417b1ec1a2096839c42821e6ebe49ee37cb86f6a
Franklin <franklin.toussaint@gmail.com>
2019-01-03 15:47:39 +0100

    Update F_Backdrop.py

--- python/Franklin/F_Backdrop.py
+++ python/Franklin/F_Backdrop.py
@@ -1,6 +1,7 @@
 '''
 I wrote this script inspired by that of Timur Khodzhaev. http://www.nukepedia.com/python/nodegraph/autobackdrop-replacement/finishdown?miv=1&mjv=2
 
+Version 1.0
 Copyright (c) 2018 Franklin VFX Co.
 '''
 
